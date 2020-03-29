fa786a8a542ab32bc7d49f4ee04634b5f8400af7
Chakradar Raju <chakradarraju@strings.ai>
2020-03-29 18:01:31 +0530

    Updates

---
+++ precache-manifest.edde4d83fea2b54c7d00eedc13964d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d892d2aa8c15f815b8028dc437e2196e",
+    "url": "/4inarow/index.html"
+  },
+  {
+    "revision": "e82d2e3fa43e987c9579",
+    "url": "/4inarow/static/css/main.6951a830.chunk.css"
+  },
+  {
+    "revision": "31e73524a890775a69d5",
+    "url": "/4inarow/static/js/2.4ffe731e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/4inarow/static/js/2.4ffe731e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82d2e3fa43e987c9579",
+    "url": "/4inarow/static/js/main.31427391.chunk.js"
+  },
+  {
+    "revision": "c4ef83d1866820c99e82",
+    "url": "/4inarow/static/js/runtime-main.19a8f634.js"
+  }
+]);
