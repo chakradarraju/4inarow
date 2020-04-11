29b93dd312477d1650a8ba5d5b00450064f90f85
Chakradar Raju <chakradarraju@strings.ai>
2020-04-11 14:33:34 +0530

    Updates

--- precache-manifest.06aefe10f9d3f0aa5f3c22d19c18a1e8.js
+++ precache-manifest.06aefe10f9d3f0aa5f3c22d19c18a1e8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0f3a14efb943db80b19ad832d3b44b68",
+    "revision": "33187142a0d5e5896ff728ee138efc5f",
     "url": "/4inarow/index.html"
   },
   {
-    "revision": "4a0701e50e286c77b448",
+    "revision": "5d882600a37e573e01b0",
     "url": "/4inarow/static/css/main.62551ea6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/4inarow/static/js/2.01074ed3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4a0701e50e286c77b448",
-    "url": "/4inarow/static/js/main.4c0a3d1b.chunk.js"
+    "revision": "5d882600a37e573e01b0",
+    "url": "/4inarow/static/js/main.084c1f99.chunk.js"
   },
   {
     "revision": "c4ef83d1866820c99e82",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/4inarow/precache-manifest.db6fa4902ad7e3aa57b027eb2f4c765c.js"
+  "/4inarow/precache-manifest.06aefe10f9d3f0aa5f3c22d19c18a1e8.js"
 );
 
 self.addEventListener('message', (event) => {
